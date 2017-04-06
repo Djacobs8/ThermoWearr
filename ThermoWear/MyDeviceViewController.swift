4ede535307407078cf8c859b850147c5e7cc0368
Derek Jacobs <dbjacobs8@gmail.com>
2017-04-06 01:27:40 -0400

    Fully functioning

--- ThermoWear/MyDeviceViewController.swift
+++ ThermoWear/MyDeviceViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MyDeviceViewController: UIViewController {
     
@@ -26,12 +27,25 @@ class MyDeviceViewController: UIViewController {
     
     @IBAction func submitTapped(_ sender: Any) {
         
-        
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // constant to be used
-        let time = Date() //constant to then be used as the date/ time
+        let time = Date()-(60*60*4) //constant to then be used as the date/ time, minus is adjustment for time zone
         let reading = Reading(context: context)
         reading.temp = Double(tempTextField.text!)!
         reading.time = time as NSDate
+        
+        let tempsFolder = FIRStorage.storage().reference().child("temps")
+        let input = reading.temp
+        var value = input
+        let data = Data(buffer: UnsafeBufferPointer(start: &value, count: 1))
+        tempsFolder.child("\(reading.temp) taken at \(reading.time)").put(data, metadata: nil, completion: {(metadata, error) in
+            print ("We tried to upload")
+            if error != nil {
+                print ("We have an error \(error)")
+            } else {
+                print(metadata?.downloadURL())
+                
+            }// adding Firebase data push
+        
         (UIApplication.shared.delegate as! AppDelegate).saveContext() // saves in core data
         if (reading.temp >= 40.0) || (reading.temp <= 35.0) {
             
@@ -42,11 +56,12 @@ class MyDeviceViewController: UIViewController {
             self.present(alertController, animated: true, completion: nil)
             
         } else {
-            dismiss(animated: true, completion: nil)
+            self.dismiss(animated: true, completion: nil)
 
         }
         
     //need to add switch for fahrenheit
     
 }
+)}
 }
